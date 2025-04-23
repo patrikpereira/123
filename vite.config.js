b3708319f6ba7393dd0ba1527aebba91ce49c7d4
Patrik Pereira <162177442+patrikpereira@users.noreply.github.com>
2025-04-22 23:58:12 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./", // 👈 importante para build no Vercel/Netlify
+  build: {
+    rollupOptions: {
+      external: ["xlsx", "file-saver"], // 👈 impede o erro
+    },
+  },
 });
